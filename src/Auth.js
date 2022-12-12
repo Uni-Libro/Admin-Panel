67e403a80cae8cf25b0b2e04c65c120716ed9c9f
Mohammad Mahdi Daghighi <mohammadmahdi.daghighi81@gmail.com>
2022-12-12 19:36:03 +0330

    implement login page completed

---
+++ src/Auth.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+export default function (props) {
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Username</label>
+            <input
+              type="username"
+              className="form-control mt-1"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn-grad">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
